72d58c2f876abef989b785171d30b4e76eb8397a
Nick-Liou <80918856+Nick-Liou@users.noreply.github.com>
2023-12-01 23:13:28 +0200

    Maybe now working

--- wide_vadd_kernels/src/kernel.cpp
+++ wide_vadd_kernels/src/kernel.cpp
@@ -1,46 +1,3 @@
-/**********
-Copyright (c) 2019, Xilinx, Inc.
-All rights reserved.
-
-Redistribution and use in source and binary forms, with or without modification,
-are permitted provided that the following conditions are met:
-
-1. Redistributions of source code must retain the above copyright notice,
-this list of conditions and the following disclaimer.
-
-2. Redistributions in binary form must reproduce the above copyright notice,
-this list of conditions and the following disclaimer in the documentation
-and/or other materials provided with the distribution.
-
-3. Neither the name of the copyright holder nor the names of its contributors
-may be used to endorse or promote products derived from this software
-without specific prior written permission.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
-THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-ARE DISCLAIMED.
-IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
-INDIRECT,
-INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-LIMITED TO,
-PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
-BUSINESS INTERRUPTION)
-HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-LIABILITY,
-OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
-THIS SOFTWARE,
-EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-**********/
-
-/*******************************************************************************
-Description:
-    HLS pragmas can be used to optimize the design : improve throughput, reduce
-latency and
-    device resource utilization of the resulting RTL code
-    This is vector addition example to demonstrate how HLS optimizations are
-used in kernel.
-*******************************************************************************/
 
 
 //Including to use ap_uint<> datatype
@@ -49,7 +6,7 @@ used in kernel.
 #include "../../wide_vadd/src/definitions.hpp"
 
 typedef ap_uint<512> uint512_dt;
-#define VECTOR_SIZE (DATAWIDTH / 32) // vector size is 16 (512/32 = 16)
+#define VECTOR_SIZE (DATAWIDTH / 32) // j size is 16 (512/32 = 16)
 
 /*
     Matrix Multiplication Kernel Implementation
@@ -60,9 +17,9 @@ typedef ap_uint<512> uint512_dt;
 */
 
 extern "C" {
-void MATRIX_MUL(const 	uint512_dt A[n * m] , 	// Read-Only Matrix A
-				const	uint512_dt B[m * p] , 	// Read-Only Matrix B
-						uint512_dt C[n * p]	// Output Result Matrix C = A*B
+void MATRIX_MUL(const 	uint512_dt A[n * m / VECTOR_SIZE] , 	// Read-Only Matrix A
+				const	uint512_dt B[m * p / VECTOR_SIZE] , 	// Read-Only Matrix B
+						uint512_dt C[n * p / VECTOR_SIZE]		// Output Result Matrix C = A*B
 					){
 
 	// ========================= OURS ========================= //
@@ -74,9 +31,9 @@ void MATRIX_MUL(const 	uint512_dt A[n * m] , 	// Read-Only Matrix A
 	#pragma HLS INTERFACE s_axilite port = C bundle = control
 	#pragma HLS INTERFACE s_axilite port = return bundle = control
 
-	uint512_dt ker_A [n*m /VECTOR_SIZE ];
-	uint512_dt ker_B [m*p /VECTOR_SIZE ];
-	uint512_dt ker_C [n*p /VECTOR_SIZE ];
+	uint512_dt ker_A [n*m / VECTOR_SIZE ];
+	uint512_dt ker_B [m*p / VECTOR_SIZE ];
+	// uint512_dt ker_C [n*p / VECTOR_SIZE ];
 
 
 	copyA:
@@ -101,15 +58,25 @@ void MATRIX_MUL(const 	uint512_dt A[n * m] , 	// Read-Only Matrix A
 				#pragma HLS PIPELINE II=1
 
 				num_t_res temp = 0 ;
+				// uint512_dt tmpV1 = v1_local[j];
+				// uint512_dt tmpV2 = v2_local[j];
+
+				// uint512_dt tmpOut = 0;
 
 				loop3:
 				for ( int j = 0 ; j < m ; j++ ){
+					// We know m = 16 
+					ap_uint<32> tmp1 = ker_A.range(i*m+  32 * (j + 1) - 1, i*m+  j * 32);
+					ap_uint<32> tmp2 = ker_B.range(k*p+  32 * (j + 1) - 1, k*p+  j * 32);
+					// tmpOut.range(32 * (j + 1) - 1, j * 32) 
+					temp += tmp1 * tmp2;
 
 
-//					temp += ker_A[i*m+j]*ker_B[j*p+k] ;
-					temp += ker_A[i*m+j]*ker_B[k*p+j] ;
+
+					// temp += ker_A[i*m+j]*ker_B[k*p+j] ;	// with B = BT transpose
+
 				}
-				C[i] = temp ;
+				C[i*p+k] = temp ;
 			}
 		}
 
